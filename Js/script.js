f82d7000b334bfcf7ca194169a91a786c5a7fc29
ayalajair <ayalajair@gmail.com>
2022-06-22 18:14:33 -0300

    Se le agregaron funcionalidades para modificar DOM

--- Js/script.js
+++ Js/script.js
@@ -7,9 +7,20 @@
 //Si se pasa de 21, se informa por alerta: ¡Perdiste!
 //Terminado el juego te pregunta si queres jugar otra vez
 //Si aceptas arranca nuevamente el juego, Si no aceptas te muestra una lista de las jugadas realizadas
-let sumadorCartas = 0;
+let sumadorCartasJugador = 0;
+let sumadorCartasCasino = 0;
 let contadorCartas= 0;
 let carta;
+let arrayCartasJugador = [];
+let arrayCartasCasino = [];
+let usuario
+
+const ingreso = document.querySelector (`#ingreso`);
+const nombre = document.querySelector (`#nombre`);
+const bienvenida = document.querySelector (`#bienvenida`);
+const inicio = document.querySelector (`#mensajeInicio`);
+const botonInicio = document.querySelector(`#botonInicio`);
+const botonReinicio = document.querySelector (`#botonReinicio`);
 //Array que contendrá el historial de jugadas
 const historialJugadas =[];
 //Clase de los objetos que se almacenarán en el Array
@@ -22,8 +33,9 @@ class Jugada {
         this.resultado="Se retiró";
         this.resumen
     }
+
 //Metodo que se fija si el Usuario perdio o no, hay que ejecutar antes de guardar en el array
-    whiteJack() {
+    esWhiteJack() {
         if (this.puntaje === 21){
         this.resultado = "¡Ganó!";
         }
@@ -36,41 +48,14 @@ class Jugada {
     }
 };
 
-
-//Función para el ingreso del nombre del jugador, si no ingresa nada le pide que lo ingrese otra vez.
-const ingresoUsuario = () =>{
-    let ingreso = true;
-    let nick;
-    while (ingreso) {
-        nick = prompt ("Ingresá tu nickname: ");
-        if (nick !=""){
-            return nick;
-        } 
-        else {
-            alert ("No ingresaste tu nombre, prueba otra vez.");
-        }
-    }
-}
-//Función para dar un mensaje de bienvenida en el HTML (Arreglar)
-const bienvenidaHtml = (nombre) => {
-    const bienvenida = document.querySelector (`#bienvenida`) 
-    bienvenida.innerHTML = `<h1> Hola ${nombre}! Vamos a jugar al White Jack!</h1>`;
-    return
-};
-
-
-//Función sumar las cartas, realiza las operaciones pertinentes del ciclo.
-const sumarCarta = (card) =>{
-        sumadorCartas = sumadorCartas + card;
-        contadorCartas++;
-}
 //Función para ver si el jugador se retira o no.
-const retira = (suma) => {
+const retira = (sumavos, sumamesa) => {
     let marcha = true;
     while (marcha) {
-        let sino = prompt (`Tenés ${suma} puntos, querés sacar otra carta? (S/N)`)
+        let sino = prompt (`Tenés ${sumavos} puntos, la casa tiene ${sumamesa}, querés sacar otra carta? (S/N)`)
         switch (sino) {
             case "s": 
+                
                 return true;
             case "S":
                 return true;
@@ -84,31 +69,23 @@ const retira = (suma) => {
 
 
 //Función para decir si se ganó o no (arreglar)
-const whiteJack = (sumador)=> {   
-    if (sumador===21) {
-        return "¡White Jack!";
-    }else {
+const whiteJack = (sumaJugador, sumaCasa)=> {  
+    if (sumaJugador>21)
+        return "¡Perdiste!"
+    else if ((sumaJugador===21) && (sumaCasa !== 21) ) 
+        return "¡Ganaste! ¡White Jack!";
+    else if ((sumaJugador===21) && (sumaCasa === 21)) 
+        return "¡Empate!";
+    else if ((sumaJugador !== 21) && (sumaCasa === 21))
         return "¡Perdiste!";
-    }
+    else if ((sumaJugador<21) && (sumaCasa > 21)) 
+        return "¡Ganaste!";
+    else if (sumaJugador>sumaCasa)
+        return "¡Ganaste!";
+    else 
+        return "¡Perdiste!";
 };
-//Función para determinar si se termina el juego o no. 
-const terminar = () =>{
-    let sigue = true;
-    while (sigue){
-        fin = prompt ("Quiere seguir jugando? (S/N)");
-        switch (fin) {
-            case "s": 
-                return true;
-            case "S":
-                return true;
-            case "n":
-                return false;
-            case "N":
-                return false;
-            default:
-                alert ("Ingrese una opción válida");
-            }  }    
-}
+
 
 //Función que realiza el informe final del total de las partidas
 
@@ -122,7 +99,10 @@ for (element of informe) {
     let jugada = document.createElement (`li`);
     jugada.innerHTML = `En esta jugada ${element.jugadas()}`
     jugadas.append(jugada);
+
 }
+
+
 //Se filtran las partidas ganadas, se guardan en un array y se contabilizan
 const ganadas = informe.filter(jugada => jugada.puntaje === 21);
 const ganados = document.querySelector (`#ganados`);
@@ -137,46 +117,107 @@ ganados.append (muestraPuntaje);
 //Juntar estos dos y hacer una funcion.
 }
 
-//Programa principal
-let usuario = ingresoUsuario();
-bienvenidaHtml (usuario);
-alert (`¡Empecemos a jugar!`)
-let jugando = true;
-while (jugando) {
-//La partida sigue mientras el jugador no se retire
-//se empiezan a generar cartas aleatorias entre 1 y 10
-//Se invoca a la funcion sumar carta, y luego a la funcion retira para ver si el jugador se retira o no
-//Cuando la suma de las cartas es igual a 21 o mas termina el juego automaticamente
-let termina = true;
-    while (termina) {
-        carta = Math.floor((Math.random()*10)+1)
-        alert (`Sacaste un ${carta}`);
-        sumarCarta (carta);
-        if (sumadorCartas >= 21){
-            break;
-        }
-        termina = retira (sumadorCartas);
+const inicioJuego = () => {
+    inicio.innerHTML = `Para empezar el Juego, hace click en el botón <b>Empezar</b>`;
+    botonInicio.style.display = "block";
+}
+
+//Funcion que inicializa el juego
+const bienvenidaUsuario = () => {
+
+    ingreso.onsubmit = (e) => {
+        e.preventDefault ();
+        usuario = nombre.value;
+        bienvenida.innerHTML = `Bienvenid@ ${usuario}!`;
+        ingreso.remove();
+        inicioJuego ();
     }
-//se informa la cantidad de cartas ingresadas
+}
+
+//Función para determinar si se termina el juego o no. 
+const terminar = () =>{
+    let sigue = true;
+    while (sigue){
+        fin = prompt ("Quiere seguir jugando? (S/N)");
+        switch (fin) {
+            case "s":
+                botonInicio.style.display= "block";
+                return true;
+            case "S":
+                botonInicio.style.display= "block";
+                return true;
+            case "n":
+                return false;
+            case "N":
+                return false;
+            default:
+                alert ("Ingrese una opción válida");
+            }  }    
+}
+
+//Programa principal
+bienvenidaUsuario ();
+botonInicio.onclick = () => {
+    inicio.remove();
+    botonInicio.style.display = "none";
+    let jugando = true;
+    while (jugando) {
+        //La partida sigue mientras el jugador no se retire
+        //se empiezan a generar cartas aleatorias entre 1 y 10
+        //Se invoca a la funcion sumar carta, y luego a la funcion retira para ver si el jugador se retira o no
+        //Cuando la suma de las cartas es igual a 21 o mas termina el juego automaticamente
+        let terminaJugador = true;
+        let terminaMesa = true;
+        while (terminaJugador || terminaMesa) {
+            if (sumadorCartasCasino<=17) {
+                carta = Math.floor((Math.random()*10)+1);
+                sumadorCartasCasino = carta + sumadorCartasCasino
+                alert (`La casa sacó ${carta}`)
+                arrayCartasCasino.push (carta);
+            }
+            else {
+                terminaMesa = false;
+            }
+            if (terminaJugador){
+                carta = Math.floor((Math.random()*10)+1);
+                alert (`Sacaste un ${carta}`);
+                arrayCartasJugador.push (carta);
+                contadorCartas ++;
+                sumadorCartasJugador = carta + sumadorCartasJugador;
+            }
+            if (sumadorCartasJugador >= 21)
+                break;
+            if (terminaJugador)
+            terminaJugador = retira (sumadorCartasJugador, sumadorCartasCasino);
+        }
+    //se informa la cantidad de cartas ingresadas
     alert (`Ingresaste: ${contadorCartas} cartas.`);
 //se llama a la funcion para ver si es whiteJack
-    alert (whiteJack(sumadorCartas));
+    alert (whiteJack(sumadorCartasJugador, sumadorCartasCasino));
 
 
 //Se crea un objeto con los datos de la jugada
-    const jugada1 = new Jugada (usuario, contadorCartas, sumadorCartas);
+const jugada1 = new Jugada (usuario, contadorCartas, sumadorCartasJugador);
 //Se llama al método para guardar si se ganó o no
-    jugada1.whiteJack();
-    jugada1.resumen = jugada1.jugadas();
+jugada1.esWhiteJack();
+jugada1.resumen = jugada1.jugadas();
 //Se guarda el objeto en el array
-    historialJugadas.push (jugada1);
+historialJugadas.push (jugada1);
 //Se llama a la funcion terminar para ver si el juego continua
     jugando = terminar ();
     contadorCartas = 0;
-    sumadorCartas = 0;
+    sumadorCartasJugador = 0;
+    sumadorCartasCasino = 0;
+    arrayCartasCasino = [];
+    arrayCartasJugador = [];
 }
-//Se llama a un funcion para hacer el informe final del total de partidas
-informeFinal (historialJugadas);
+    //Se llama a un funcion para hacer el informe final del total de partidas
+    informeFinal (historialJugadas); 
+};
+
+
+
+
 
 
 
